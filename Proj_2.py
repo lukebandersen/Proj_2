08e34a8d3396fbf9c294299a116265ea01939879
lukebandersen <lukebandersen@gmail.com>
2026-02-06 21:56:04 -0700

    User Input

--- Proj_2.py
+++ Proj_2.py
@@ -11,3 +11,11 @@ def divide(num1, num2)
 
 num1 = int(input("enter the first number:  "))
 num2 int(input("Enter the second number:  "))
+
+print("Choose your operator")
+print("1 = Addition")
+print("2 = Subtraction")
+print("3 = Multiplication")
+print("4 = Division")
+
+choice - int(input("enter you choice (1-4):  "))
