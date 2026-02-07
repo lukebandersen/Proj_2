dc0e9610f7305664f97268826b2f1b2acf32aba3
lukebandersen <lukebandersen@gmail.com>
2026-02-06 21:52:28 -0700

    user input info

--- Proj_2.py
+++ Proj_2.py
@@ -8,4 +8,6 @@ def multiply(num1, num2);
     return num1*num2
 def divide(num1, num2)
     return num1 / num2
-                                                                         
+
+num1 = int(input("enter the first number:  "))
+num2 int(input("Enter the second number:  "))
